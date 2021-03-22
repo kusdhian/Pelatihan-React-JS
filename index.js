9a0aac6557fa113d6d44f8c30278e268d826e823
kusdhian <30747685+kusdhian@users.noreply.github.com>
2021-03-22 09:37:54 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+// console.log("file terpanggil");
+
+// let playerName;
+// playerName = "Udin";
+// console.log(playerName);
+// playerName = "Andin";
+// console.log(playerName);
+
+// const pi = 3.14;
+// console.log(pi);
+
+// let berat = 75;
+// console.log(berat + 15);
+
+// let berat = 75;
+// let tinggi = 10
+// console.log(berat * tinggi);
+
+// let pesan1 = "halo, ";
+// let pesan2 = 'selamat malam Jum\'at';
+// console.log(pesan1 + pesan2);
+
+// const gabungan = `${pesan1} dan ${pesan2}`;
+// console.log(gabungan);
+
+// const name = prompt();
+// console.log("selamat datang " + name);
+
+// array
+
+// let fruits = ["anggur", "jeruk", "salak", "manggis"];
+// fruits = [];
+// fruits.push("durian", "rambutan");
+// console.log(fruits);
+// let fruits2 = ["durian", "mangga", "pisang"];
+
+// fruits.push.apply(fruits, fruits2);
+// console.log(fruits);
+
+// let fruits = ["anggur", "jeruk"];
+// fruits.pop();
+// console.log(fruits);
+
+// let fruits = ["anggur", "jeruk", "salak"];
+// fruits.splice(0, 1);
+// console.log(fruits); // jeruk, salak
+
+let fruits = ["anggur", "jeruk", "salak"];
+fruits.splice(0, 1, "manggis");
+console.log(fruits);
